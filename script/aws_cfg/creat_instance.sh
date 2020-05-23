c806f03b4f4e2a5aa7f85476a83f09be6f49c020
ChenyanWu <czw390@psu.edu>
2020-05-23 03:41:19 -0400

    update aws script

--- script/aws_cfg/creat_instance.sh
+++ script/aws_cfg/creat_instance.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-mkdir data
-cd data
-mkdir coco
-cd coco
+#install python package
+pip install -r requirements.txt
+echo done python package install
+
+#install coco api
+cd ..
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi
+make install
+cd ../nas_pose_estimation
+echo done coco api install
+
+#make lib
+cd lib
+make
+cd ..
+
+#download dataset
+mkdir data && cd data
+mkdir coco && cd coco
 mkdir person_detection_results
-mkdir annotations
 mkdir images
-#wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/train2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip val2017.zip -d images/
-unzip annotations_trainval2017.zip -d annotations/
+unzip train2017.zip -d images/
+unzip annotations_trainval2017.zip
+cd ../..
+echo done dataset install
